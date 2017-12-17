6aaa56d3690a8e94258c5f5618fa9f163c11d7b1
EncryptionBot <34613133+EncryptionBot@users.noreply.github.com>
2017-12-17 09:38:27 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = '1WhZWKXrAG0nHooi38yXieCxETFb9BKdD/d8/8CHInoH5/qHlifvexSAZ7chqABz0jjUsMgk7XATPcWD7maKkjNuXcwERUwlTjFXtqqznrgl2wNtaCPn22WbQar9vUld5iaSNyVU1b+WHngffip9twdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
